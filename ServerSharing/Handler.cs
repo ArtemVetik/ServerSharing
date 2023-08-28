649c221a2596f5410a44f036a809fb944471c2ed
Artem Vetik <artem.vetik58@gmail.com>
2023-08-28 14:04:35 +0300

    Refactoring Handler.cs

--- ServerSharing/Handler.cs
+++ ServerSharing/Handler.cs
@@ -18,39 +18,48 @@ namespace ServerSharing
 
             var token = new TokenProvider(new MetadataCredentialsProvider().GetToken());
             var config = new DriverConfig(ydbEndpoint, ydbDatabase, token);
-
-            using var driver = new Driver(config);
+            
+            var driver = new Driver(config);
             await driver.Initialize();
 
-            using var tableClient = new TableClient(driver, new TableClientConfig());
+            var tableClient = new TableClient(driver, new TableClientConfig());
             
             var awsConfig = new AmazonDynamoDBConfig()
             {
                 RegionEndpoint = RegionEndpoint.EUCentral1,
                 EndpointProvider = new EndpointProvider(),
             };
-
+            
             var awsAccessKeyId = Environment.GetEnvironmentVariable("awsAccessKeyId");
             var awsSecretAccessKey = Environment.GetEnvironmentVariable("awsSecretAccessKey");
 
-            using var awsClient = new AmazonDynamoDBClient(awsAccessKeyId, awsSecretAccessKey, awsConfig);
+            var awsClient = new AmazonDynamoDBClient(awsAccessKeyId, awsSecretAccessKey, awsConfig);
 
-            BaseRequest requestHandler = request.method switch
+            try
             {
-                "UPLOAD" => new UploadRequest(awsClient, tableClient, request),
-                "DELETE" => new DeleteRequest(awsClient, tableClient, request),
-                "LOAD_IMAGE" => new LoadImageRequest(tableClient, request),
-                "DOWNLOAD" => new DownloadRequest(tableClient, request),
-                "SELECT" => new SelectRequest(tableClient, request),
-                "LIKE" => new LikeRequest(tableClient, request),
-                "RATE" => new RateRequest(tableClient, request),
+                BaseRequest requestHandler = request.method switch
+                {
+                    "UPLOAD" => new UploadRequest(awsClient, tableClient, request),
+                    "DELETE" => new DeleteRequest(awsClient, tableClient, request),
+                    "LOAD_IMAGE" => new LoadImageRequest(tableClient, request),
+                    "DOWNLOAD" => new DownloadRequest(tableClient, request),
+                    "SELECT" => new SelectRequest(tableClient, request),
+                    "LIKE" => new LikeRequest(tableClient, request),
+                    "RATE" => new RateRequest(tableClient, request),
 #if TEST_ENVIRONMENT
                 "CLEAR" => new ClearRequest(awsClient, tableClient, request),
 #endif
-                _ => throw new InvalidOperationException($"Method {request.method} not found")
-            };
+                    _ => throw new InvalidOperationException($"Method {request.method} not found")
+                };
 
-            return await requestHandler.Handle();
+                return await requestHandler.Handle();
+            }
+            finally
+            {
+                awsClient.Dispose();
+                tableClient.Dispose();
+                driver.Dispose();
+            }
         }
     }
 }
