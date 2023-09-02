e14ee4b597dfe99dcd779f62a6cd56d5c7546413
Artem Vetik <av.unitygames@gmail.com>
2023-09-02 12:30:29 +0300

    Add Info request

--- ServerSharing/Requests/EntryTypes.cs
+++ ServerSharing/Requests/EntryTypes.cs
@@ -18,6 +18,37 @@ namespace ServerSharing
                 };
             }
 
+            internal static string CreateInfo(string id, string userId)
+            {
+                return $@"
+                    $downloads = (SELECT downloads.id, COUNT(downloads.id) as count
+                    FROM `{Tables.Downloads}` downloads
+                    GROUP BY downloads.id
+                    ORDER BY count);
+
+                    $likes = (SELECT likes.id, COUNT(likes.id) as count
+                    FROM `{Tables.Likes}` likes
+                    GROUP BY likes.id
+                    ORDER BY count);
+
+                    $myLikes = (SELECT *
+                    FROM `{Tables.Likes}` likes
+                    WHERE likes.user_id == ""{userId}"");
+
+                    $rating = (SELECT ratings.id, COUNT(ratings.id) as count, AVG(ratings.rating) as avg
+                    FROM `{Tables.Ratings}` ratings
+                    GROUP BY ratings.id);
+
+                    SELECT records.id, records.body, records.date, downloads.count, likes.count, ratings.count, ratings.avg, if (myLikes.id is null, false, true) as myLike
+                    FROM `{Tables.Records}` as records
+                    LEFT JOIN $downloads as downloads on downloads.id == records.id
+                    LEFT JOIN $likes as likes on likes.id == records.id
+                    LEFT JOIN $rating as ratings on ratings.id == records.id
+                    LEFT JOIN $myLikes as myLikes on myLikes.id == records.id
+                    WHERE records.id == ""{id}""
+                ";
+            }
+
             private static string All(string userId)
             {
                 return $@"
