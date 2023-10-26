b7fa7e2e6ac6a18691aa6c1d26ac08b3fbfddff1
Artem Vetik <av.unitygames@gmail.com>
2023-10-26 17:11:21 +0300

    Refactoring SELECT and INFO query factory

--- ServerSharing/Requests/SelectQuery/SelectQueryContainer.cs
+++ ServerSharing/Requests/SelectQuery/SelectQueryContainer.cs
@@ -4,9 +4,9 @@ namespace ServerSharing
 {
     internal partial class SelectQueryFactory
     {
-        private static class EntryTypes
+        private class SelectQueryContainer
         {
-            internal static string Create(EntryType type, string userId)
+            public static string Create(EntryType type, string userId)
             {
                 return type switch
                 {
@@ -16,51 +16,6 @@ namespace ServerSharing
                     EntryType.Liked => Liked(userId),
                     _ => throw new NotSupportedException()
                 };
-            }
-
-            internal static string CreateInfo(string id, string userId)
-            {
-                return $@"
-                    $downloads = (SELECT downloads.id, COUNT(downloads.id) as count
-                    FROM `{Tables.Downloads}` downloads
-                    GROUP BY downloads.id
-                    ORDER BY count);
-
-                    $likes = (SELECT likes.id, COUNT(likes.id) as count
-                    FROM `{Tables.Likes}` likes
-                    GROUP BY likes.id
-                    ORDER BY count);
-
-                    $myLikes = (SELECT *
-                    FROM `{Tables.Likes}` likes
-                    WHERE likes.user_id == ""{userId}"");
-
-                    $rating = (SELECT ratings.id, COUNT(ratings.id) as count, AVG(ratings.rating) as avg
-                    FROM `{Tables.Ratings}` ratings
-                    GROUP BY ratings.id);
-                    
-                    $myRating = (SELECT *
-                    FROM `{Tables.Ratings}` ratings
-                    WHERE ratings.user_id == ""{userId}"");
-
-                    $myDownloads = (SELECT *
-                    FROM `{Tables.Downloads}` downloads
-                    WHERE downloads.user_id == ""{userId}"");
-
-                    SELECT records.id, records.body, records.date, downloads.count, likes.count, ratings.count, ratings.avg,
-                        if (myLikes.id is null, false, true) as myLike,
-                        if (myRating.id is null, null, myRating.rating) as myRating,
-                        if (myDownloads.id is null, false, true) as myDownload,
-                        if (records.user_id == ""{userId}"", true, false) as myRecord
-                    FROM `{Tables.Records}` as records
-                    LEFT JOIN $downloads as downloads on downloads.id == records.id
-                    LEFT JOIN $likes as likes on likes.id == records.id
-                    LEFT JOIN $rating as ratings on ratings.id == records.id
-                    LEFT JOIN $myLikes as myLikes on myLikes.id == records.id
-                    LEFT JOIN $myRating as myRating on myRating.id == records.id
-                    LEFT JOIN $myDownloads as myDownloads on myDownloads.id == records.id
-                    WHERE records.id == ""{id}""
-                ";
             }
 
             private static string All(string userId)
