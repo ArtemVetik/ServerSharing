2727da2d5b8f0d3e0691cafa8be698ac048c9d27
Artem Vetik <av.unitygames@gmail.com>
2023-10-29 13:17:23 +0300

    Fix queries affecting `count` attributes in the records table

--- ServerSharing/Requests/LikeRequest.cs
+++ ServerSharing/Requests/LikeRequest.cs
@@ -19,6 +19,34 @@ namespace ServerSharing
                     DECLARE $user_id AS string;
                     DECLARE $id AS string;
 
+                    SELECT id
+                    FROM `{Tables.Likes}`
+                    WHERE user_id = $user_id AND id = $id;
+                ";
+
+                return await session.ExecuteDataQuery(
+                    query: query,
+                    txControl: TxControl.BeginSerializableRW().Commit(),
+                    parameters: new Dictionary<string, YdbValue>
+                    {
+                        { "$user_id", YdbValue.MakeString(Encoding.UTF8.GetBytes(request.user_id)) },
+                        { "$id", YdbValue.MakeString(Encoding.UTF8.GetBytes(request.body)) },
+                    }
+                );
+            });
+
+            var queryResponse = (ExecuteDataQueryResponse)response;
+            var resultSet = queryResponse.Result.ResultSets[0];
+
+            if (resultSet.Rows.Count != 0)
+                return new Response((uint)response.Status.StatusCode, response.Status.StatusCode.ToString(), string.Empty);
+
+            response = await client.SessionExec(async session =>
+            {
+                var query = $@"
+                    DECLARE $user_id AS string;
+                    DECLARE $id AS string;
+
                     INSERT INTO `{Tables.Likes}` (user_id, id)
                     VALUES ($user_id, $id);
 
--- ServerSharing/Requests/RateRequest.cs
+++ ServerSharing/Requests/RateRequest.cs
@@ -28,6 +28,11 @@ namespace ServerSharing
                 throw new InvalidOperationException("Request body has an invalid format", exception);
             }
 
+            var rated = await AlreadyRated(client, request.user_id, body.Id);
+            
+            if (rated == false)
+                await IncreaseCount(client, body.Id);
+            
             var response = await client.SessionExec(async session =>
             {
                 var query = $@"
@@ -35,19 +40,15 @@ namespace ServerSharing
                     DECLARE $id AS string;
                     DECLARE $rating AS int8;
 
-                    $avg_rating = (SELECT CAST(AVG(ratings.rating) as float?)
-                    from `{Tables.Ratings}` VIEW idx_id ratings
-                    where ratings.id = $id);
-
-                    INSERT INTO `{Tables.Ratings}` (user_id, id, rating)
+                    UPSERT INTO `{Tables.Ratings}` (user_id, id, rating)
                     VALUES ($user_id, $id, $rating);
 
-                    UPDATE `{Tables.Ratings}`
-                    SET rating_count = if (rating_count is null, 1u, rating_count + 1u)
-                    WHERE id = $id;
+                    $avg_rating = (SELECT CAST(AVG(ratings.rating) AS float?)
+                    FROM `{Tables.Ratings}` VIEW idx_id ratings
+                    WHERE ratings.id = $id);
 
                     UPDATE `{Tables.Records}`
-                    SET rating_avg = if (rating_avg is null, 3, $avg_rating)
+                    SET rating_avg = if (rating_avg is null, $rating, $avg_rating)
                     WHERE id = $id;
                 ";
 
@@ -64,6 +65,61 @@ namespace ServerSharing
             });
 
             return new Response((uint)response.Status.StatusCode, response.Status.StatusCode.ToString(), string.Empty);
+        }
+
+        private async Task<bool> AlreadyRated(TableClient client, string userId, string id)
+        {
+            var response = await client.SessionExec(async session =>
+            {
+                var query = $@"
+                    DECLARE $user_id AS string;
+                    DECLARE $id AS string;
+
+                    SELECT id
+                    FROM `{Tables.Ratings}`
+                    WHERE user_id = $user_id AND id = $id;
+                ";
+
+                return await session.ExecuteDataQuery(
+                    query: query,
+                    txControl: TxControl.BeginSerializableRW().Commit(),
+                    parameters: new Dictionary<string, YdbValue>
+                    {
+                        { "$user_id", YdbValue.MakeString(Encoding.UTF8.GetBytes(userId)) },
+                        { "$id", YdbValue.MakeString(Encoding.UTF8.GetBytes(id)) },
+                    }
+                );
+            });
+
+            var queryResponse = (ExecuteDataQueryResponse)response;
+            var resultSet = queryResponse.Result.ResultSets[0];
+
+            var increateRatingCount = string.Empty;
+
+            return resultSet.Rows.Count != 0;
+        }
+
+        private async Task IncreaseCount(TableClient client, string id)
+        {
+            await client.SessionExec(async session =>
+            {
+                var query = $@"
+                    DECLARE $id AS string;
+
+                    UPDATE `{Tables.Records}`
+                    SET rating_count = if (rating_count is null, 1u, rating_count + 1u)
+                    WHERE id = $id
+                ";
+
+                return await session.ExecuteDataQuery(
+                    query: query,
+                    txControl: TxControl.BeginSerializableRW().Commit(),
+                    parameters: new Dictionary<string, YdbValue>
+                    {
+                        { "$id", YdbValue.MakeString(Encoding.UTF8.GetBytes(id)) },
+                    }
+                );
+            });
         }
     }
 }
